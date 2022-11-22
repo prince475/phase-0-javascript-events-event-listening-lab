a0fd9de147d4bd946fb69c6a25ac79b8e78a7936
prince475 <danielmwai475@gmail.com>
2022-11-22 22:41:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+//creating event listener using addEventListener using  event to listen for, and a callback function to "handle" the event
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+    alert('I was clicked!');
+});
 }
+//you will need to call the outer function in index.js to execute addEventListener() and activate the event listener
+addingEventListener()
